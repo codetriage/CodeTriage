cccacf07f04d1e9caca6ded8f01afcd626d96c17
schneems <richard.schneeman@gmail.com>
2012-06-22 15:28:55 -0500

    validate github url exists when creating the repo

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,5 +1,19 @@
 class Repo < ActiveRecord::Base
 
+  validate :github_url_exists, :on => :create
+
+
+  def github_url_exists
+    response = HTTParty.get(github_url)
+    if response.code != 200
+      errors.add(:expiration_date, "cannot reach #{github_url} perhaps github is down?")
+    end
+  end
+
+  def github_url
+    File.join("http://github.com", username_repo)
+  end
+
   def username_repo
     "#{user_name}/#{name}"
   end
