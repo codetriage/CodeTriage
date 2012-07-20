05440f21cc560decfbaf1bdc2ba5d51ac81691d2
schneems <richard.schneeman@gmail.com>
2012-07-20 17:33:35 -0500

    show repos with the most issues on the front page

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'will_paginate'
 
 gem 'httparty'
 
+gem 'thin'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def index
+    @repos = Repo.joins(:issues).group('issues.id', 'repos.id').order('COUNT(issues)').includes(:issues).first(50)
   end
 end
