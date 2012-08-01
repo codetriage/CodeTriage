cd0ac7befa5b03cc007e06fb7c51184cf1dee8fa
schneems <richard.schneeman@gmail.com>
2012-08-01 13:56:52 -0500

    fix url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Example::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  config.action_mailer.default_url_options = { :host => "http://issuetriage.heroku.com" }
+  config.action_mailer.default_url_options = { :host => "issuetriage.heroku.com" }
 
 
   # Generate digests for assets URLs
