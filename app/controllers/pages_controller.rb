72dfe24118ea6e03baf43236de6c45f59dc4e104
schneems <richard.schneeman@gmail.com>
2012-09-17 17:48:10 -0500

    initial commit how page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def index
     @repos = Repo.order_by_issue_count
   end
+
+  def show
+    render "pages/show/#{params[:id]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Example::Application.routes.draw do
 
   devise_for  :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 
-
+  resources   :pages
   root        :to => "pages#index"
 
 
