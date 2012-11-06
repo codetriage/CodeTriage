fcf07ceb734303de3c350d78f2916c604eb40b12
schneems <richard.schneeman@gmail.com>
2012-11-06 00:53:25 -0600

    fix tests

--- test/unit/repo_subscriptions_test.rb
+++ test/unit/repo_subscriptions_test.rb
@@ -2,7 +2,7 @@
 
 class RepoSubscriptionsTest < ActiveSupport::TestCase
 
-  test "the issue_for_triage! for new user" do
+  test "the get_issue_for_triage for new user" do
     user     = User.create(:email => Time.now.to_f.to_s + "foo@example.com" , :password => Devise.friendly_token[0,20])
     repo     = Repo.create(:user_name => 'rails', :name => 'rails')
     repo_sub = user.repo_subscriptions.create(:repo => repo)
@@ -13,12 +13,12 @@ class RepoSubscriptionsTest < ActiveSupport::TestCase
                        :state           => 'open',
                        :html_url        => "http://schneems.com")
 
-    issue = repo_sub.issue_for_triage!
+    issue = repo_sub.get_issue_for_triage
     assert issue.is_a? Issue
   end
 
 
-  test "the issue_for_triage! for user with existing issue assignments" do
+  test "the get_issue_for_triage for user with existing issue assignments" do
     user     = User.create(:email => Time.now.to_f.to_s + "foo@example.com" , :password => Devise.friendly_token[0,20])
     repo     = Repo.create(:user_name => 'rails', :name => 'rails')
     repo_sub = user.repo_subscriptions.create(:repo => repo)
@@ -38,7 +38,7 @@ class RepoSubscriptionsTest < ActiveSupport::TestCase
 
     repo_sub.issue_assignments.create(:issue => assigned_issue)
 
-    issue = repo_sub.issue_for_triage!
+    issue = repo_sub.get_issue_for_triage
     assert issue.is_a? Issue
   end
 
