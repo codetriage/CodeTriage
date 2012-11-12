d613bc8349915564333ca0c16f39d65a0ae32acc
schneems <richard.schneeman@gmail.com>
2012-11-12 15:31:57 +0200

    set correct port to comply with foreman

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Example::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:5000' }
 end
