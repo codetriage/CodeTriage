bfcb8228b479b91f55a93365f60f0f5b302ffb44
schneems <richard.schneeman@gmail.com>
2012-11-19 15:14:29 -0600

    explain github public email exception

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.github_data"] = request.env["omniauth.auth"]
+      flash[:error] = no_email_error if request.env["omniauth.auth"].info.email.blank?
       redirect_to root_path
     end
+  end
+
+  private
+
+  def no_email_error
+    msg =  "You need a public email address on Github to sign up"
+    msg << "you can add an email, sign up for triage, then remove it from Github:<hr />"
+    msg << "<a href='https://github.com/settings/profile'>Github Profile</a>"
+    msg.html_safe
   end
 end
