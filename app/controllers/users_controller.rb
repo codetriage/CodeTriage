d86a6b216a7d048cf5c5d34599590217a9c45112
schneems <richard.schneeman@gmail.com>
2012-12-04 12:26:57 -0600

    add missing destroy action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_filter :authenticate_user!, :only => :update
+  before_filter :authenticate_user!, :only => [:update, :destroy]
 
 
   def show
@@ -11,5 +11,11 @@ class UsersController < ApplicationController
     @user = current_user
     @user.update_attributes(params[:user])
     redirect_to :back
+  end
+
+  def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to root_url
   end
 end
