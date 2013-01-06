0e66245f5913db0c5884c38aa4f33d9138dc7a2d
schneems <richard.schneeman@gmail.com>
2013-01-06 12:57:34 -0600

    Make Application.config.secret_token secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Example::Application.config.secret_token = 'ab37b1294efd8458ee5f186d1164e91bb234e123b8244fbc009f50b223a655f2b4265e519b93db715d9b3c8a382b7fe961ef9b2857105f14efaf32ce72f52e9e'
+Example::Application.config.secret_token = ENV['SECRET_TOKEN']||'ab37b1294efd8458ee5f186d1164e91bb234e123b8244fbc009f50b223a655f2b4265e519b93db715d9b3c8a382b7fe961ef9b2857105f14efaf32ce72f52e9e'
