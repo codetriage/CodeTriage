b87f58bb749715a5f59dad8b6fff23101ef6e869
schneems <richard.schneeman@gmail.com>
2013-02-07 12:47:41 -0600

    Requesting user email scope on GH OAuth
    Currently if someone only has a private email address, this should fix that scope issue, thanks to this HN thread: http://news.ycombinator.com/item?id=5183684

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -210,7 +210,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], :scope => ''
+  config.omniauth :github, ENV['GITHUB_APP_ID'], ENV['GITHUB_APP_SECRET'], :scope => 'user:email'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
