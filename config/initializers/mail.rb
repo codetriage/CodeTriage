c5e8883058333e3b538c0c5813afedbe51ac115a
Jon Rowe <hello@jonrowe.co.uk>
2013-03-03 12:06:27 +1100

    enable local testing/sending of email

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'codetriage.com'
-}
-ActionMailer::Base.delivery_method = :smtp
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'codetriage.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+end
