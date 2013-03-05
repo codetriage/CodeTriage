bfdeb079a52dd083823bbd64c11a82d3afcf06c4
schneems <richard.schneeman@gmail.com>
2013-03-05 17:22:40 -0600

    [fix #85] validate => validates
    This fills me with rage. That `s` makes a pretty big difference.

--- app/models/repo_subscription.rb
+++ app/models/repo_subscription.rb
@@ -1,5 +1,6 @@
 class RepoSubscription < ActiveRecord::Base
-  validate :repo_id, :uniqueness => {:scope => :user_id}
+  validates :repo_id, :uniqueness => {:scope => :user_id}
+
   belongs_to :repo
   belongs_to :user
   has_many   :issue_assignments
