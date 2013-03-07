807e7b53b1b45b31101b121611d9c8f925491a96
schneems <richard.schneeman@gmail.com>
2013-03-07 12:26:11 -0600

    return nil, so we can compact arrays of issues

--- app/models/repo_subscription.rb
+++ app/models/repo_subscription.rb
@@ -37,7 +37,7 @@ class RepoSubscription < ActiveRecord::Base
 
   def get_issue_for_triage
     issue = repo.issues.where(:state => 'open').where("id not in (?)", assigned_issue_ids).all.sample
-    return false if issue.blank?
+    return nil   if issue.blank?
     return issue if issue.valid_for_user?(self.user)
 
     # add issue to restricted list and try again
