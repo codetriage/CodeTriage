e50bcc61c03fbadf9f262356e4c12589b6475cab
schneems <richard.schneeman@gmail.com>
2013-04-02 10:16:42 -0500

    set default_url_options[:host] for test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,6 @@ Example::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { :host => 'example.com' }
 end
