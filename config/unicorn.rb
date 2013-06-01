47b551c3f241517fc218b05080363c2b02be583b
schneems <richard.schneeman@gmail.com>
2013-06-01 11:49:10 +0900

    switching to OPEN_REDIS

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -28,8 +28,8 @@ after_fork do |server, worker|
 
   # If you are using Redis but not Resque, change this
   if defined?(Resque)
-    ENV["REDISTOGO_URL"] ||= "redis://127.0.0.1:6379"
-    uri = URI.parse(ENV["REDISTOGO_URL"])
+    ENV["OPENREDIS_URL"] ||= "redis://127.0.0.1:6379"
+    uri = URI.parse(ENV["OPENREDIS_URL"])
     Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
     Rails.logger.info('Connected to Redis')
   end
