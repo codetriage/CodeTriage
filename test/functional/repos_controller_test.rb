b00b7832e935cf24b8a15e1460aed4cec13c056a
Vlad GURDIGA <gurdiga@gmail.com>
2013-06-16 19:07:33 -0400

    Respond with HTTP 404 when the repo is not registered

--- test/functional/repos_controller_test.rb
+++ test/functional/repos_controller_test.rb
@@ -1,5 +1,13 @@
 require 'test_helper'
 
 class ReposControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
 
+  fixtures :users, :repos
+
+  test 'responds with 404 if repo does not exist' do
+    assert_raise(ActiveRecord::RecordNotFound) {
+      get :show, user_name: 'foo', name: 'bar'
+    }
+  end
 end
