078bc5cf140443b22fc727f87465c64dab0d2a37
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-09-16 23:01:42 +0530

    Remove unused action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,10 +7,6 @@ class PagesController < ApplicationController
     @repos_subs = current_user.repo_subscriptions.page(params[:page]||1).per_page(params[:per_page]||50) if user_signed_in?
   end
 
-  def show
-    render "pages/show/#{params[:id]}"
-  end
-
   private
 
   def get_layout
