517510c7070312dc4c102c1f0466d66c3503887e
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-09-18 18:15:58 +0530

    User can skip issues attached with pull requests by setting skip_issues_with_pr to true

--- test/integration/user_update_test.rb
+++ test/integration/user_update_test.rb
@@ -31,4 +31,26 @@ class UserUpdateTest < ActionController::IntegrationTest
     assert_nil @user.daily_issue_limit
     assert_routing user_path(@user), { :controller => 'users', :action => 'show', :id => '1' }
   end
+
+  test 'updating the users skip_issues_with_pr setting to true' do
+    @user = User.first
+    visit edit_user_path(@user)
+    check 'user_skip_issues_with_pr'
+    click_button 'Save'
+    assert page.has_content?('User successfully updated')
+    @user.reload
+    assert @user.skip_issues_with_pr
+    assert_routing user_path(@user), { :controller => 'users', :action => 'show', :id => '1' }
+  end
+
+  test 'updating the users skip_issues_with_pr setting to false' do
+    @user = User.first
+    visit edit_user_path(@user)
+    uncheck 'user_skip_issues_with_pr'
+    click_button 'Save'
+    assert page.has_content?('User successfully updated')
+    @user.reload
+    assert_equal false, @user.skip_issues_with_pr
+    assert_routing user_path(@user), { :controller => 'users', :action => 'show', :id => '1' }
+  end
 end
