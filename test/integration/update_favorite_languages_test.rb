4eceb4a1be0542efac49fd26e4bcb25d79d19215
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-10-30 22:16:09 +0200

    Fix unchecking favorite languages
    Fix error `ActionController::ParameterMissing (param not found: user)`
    when user unchecks favorite languages.

---
+++ test/integration/update_favorite_languages_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UpdateFavoriteLanguagesTest < ActionController::IntegrationTest
+  setup do
+    @user = User.first
+    @user.favorite_languages = ["ruby"]
+    @user.save
+
+    visit user_path @user
+  end
+
+  test 'clears favorite languages' do
+    uncheck 'user_favorite_languages_ruby'
+    click_button 'Save'
+    @user.reload
+    assert_equal [], @user.favorite_languages
+    assert page.has_content? 'User successfully updated'
+  end
+
+  test 'adds more favorite languages' do
+    check 'user_favorite_languages_javascript'
+    click_button 'Save'
+    @user.reload
+    assert_equal ["javascript", "ruby"], @user.favorite_languages
+    assert page.has_content? 'User successfully updated'
+  end
+end
