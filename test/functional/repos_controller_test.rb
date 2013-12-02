d5cc8249b211ba12aaac24e70bbf7157799d04ac
James Cook <james@isotope11.com>
2013-12-02 08:37:38 -0600

    Add test for ReposController#new

--- test/functional/repos_controller_test.rb
+++ test/functional/repos_controller_test.rb
@@ -10,4 +10,21 @@ class ReposControllerTest < ActionController::TestCase
       get :show, full_name: 'foo/bar'
     }
   end
+
+  test 'finds own, starred and watched repos on #new' do
+    fake_repo_response         = stub(json_body: {})
+    fake_starred_repo_response = stub(json_body: {})
+    fake_subscription_response = stub(json_body: {})
+
+    GitHubBub.expects(:get).with('/user/repos', token: nil, type: 'owner', per_page: '100').returns(fake_repo_response)
+    GitHubBub.expects(:get).with('/user/starred', token: nil).returns(fake_starred_repo_response)
+    GitHubBub.expects(:get).with('/user/subscriptions', token: nil).returns(fake_subscription_response)
+
+    sign_in users(:mockstar)
+    get :new
+
+    assert_not_nil assigns(:own_repos)
+    assert_not_nil assigns(:starred_repos)
+    assert_not_nil assigns(:watched_repos)
+  end
 end
