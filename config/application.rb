9ec7e21464e1df4ec1bfd930a5f9298ff411fbb2
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-12-22 15:48:02 +0530

    Moved parameter filtering in initializers
    - Running rake rails:update generated filter_parameter_logging.rb in initializers
    - So removed this setting from application.rb

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,6 @@ module Example
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
