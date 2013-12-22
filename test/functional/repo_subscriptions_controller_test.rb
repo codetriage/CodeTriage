da4687d39c6d6ec56d700fbcb0b2b1225330f02a
Arthur Neves <arthurnn@gmail.com>
2013-12-22 15:24:13 -0500

    Replace context(specs) from repo_subscription test

--- test/functional/repo_subscriptions_controller_test.rb
+++ test/functional/repo_subscriptions_controller_test.rb
@@ -9,62 +9,56 @@ class RepoSubscriptionsControllerTest < ActionController::TestCase
     request.env["HTTP_REFERER"] = "http://localhost:5000"
   end
 
-  context 'when signed in as mockstar' do
-    setup do
-      sign_in users(:mockstar)
-    end
+  test 'successfully subscribe to a repo when signed in as mockstart' do
+    sign_in users(:mockstar)
+    post :create, repo_id: repos(:rails_rails).id
+    assert flash[:notice] == I18n.t('repo_subscriptions.subscribed')
+    assert_redirected_to repo_subscriptions_path
+  end
 
-    should 'successfully subscribe to a repo' do
-      post :create, repo_id: repos(:rails_rails).id
-      assert flash[:notice] == I18n.t('repo_subscriptions.subscribed')
-      assert_redirected_to repo_subscriptions_path
-    end
+  test 'recive failure message when subscription did not created when signed in as mockstart' do
+    sign_in users(:mockstar)
+    RepoSubscription.any_instance.stubs(:save).returns false
+    post :create, repo_id: repos(:rails_rails).id
+    assert_equal flash[:error], "Something went wrong"
+    assert_redirected_to :back
+  end
 
-    should 'recive failure message when subscription did not created' do
-      RepoSubscription.any_instance.stubs(:save).returns false
-      post :create, repo_id: repos(:rails_rails).id
-      assert_equal flash[:error], "Something went wrong"
-      assert_redirected_to :back
-    end
-
-    should "not update schneems' subscription" do
-      assert_raise ActiveRecord::RecordNotFound do
-        patch :update, id: repo_subscriptions(:schneems_to_triage).id,
-          repo_subscription: { email_limit: 12 }
-      end
-    end
-
-    should "not destroy schneems' subscription" do
-      assert_raise ActiveRecord::RecordNotFound do
-        delete :destroy, id: repo_subscriptions(:schneems_to_triage).id
-      end
+  test "not update schneems' subscription when signed in as mockstart" do
+    sign_in users(:mockstar)
+    assert_raise ActiveRecord::RecordNotFound do
+      patch :update, id: repo_subscriptions(:schneems_to_triage).id,
+      repo_subscription: { email_limit: 12 }
     end
   end
 
-  context 'when signed in as schneems' do
-    setup do
-      sign_in users(:schneems)
-    end
-
-    should 'destroy a subscription' do
+  test "not destroy schneems' subscription when signed in as mockstart" do
+    sign_in users(:mockstar)
+    assert_raise ActiveRecord::RecordNotFound do
       delete :destroy, id: repo_subscriptions(:schneems_to_triage).id
-      assert_redirected_to :back
-      assert_equal 1, RepoSubscription.count
     end
+  end
 
-    should 'update subscription limit when email limit is valid' do
-      patch :update, id: repo_subscriptions(:schneems_to_triage).id,
-        repo_subscription: { email_limit: 12 }
-      assert_equal flash[:success], "Email preferences updated!"
-      assert_redirected_to :back
-    end
+  test 'destroy a subscription' do
+    sign_in users(:schneems)
+    delete :destroy, id: repo_subscriptions(:schneems_to_triage).id
+    assert_redirected_to :back
+    assert_equal 1, RepoSubscription.count
+  end
 
-    should 'not update subscription when email limit is invalid' do
-      patch :update, id: repo_subscriptions(:schneems_to_triage).id,
-        repo_subscription: { email_limit: 0 }
-      assert_equal flash[:error], "Something went wrong"
-      assert_redirected_to :back
-    end
+  test 'update subscription limit when email limit is valid' do
+    sign_in users(:schneems)
+    patch :update, id: repo_subscriptions(:schneems_to_triage).id,
+    repo_subscription: { email_limit: 12 }
+    assert_equal flash[:success], "Email preferences updated!"
+    assert_redirected_to :back
+  end
+
+  test 'not update subscription when email limit is invalid' do
+    sign_in users(:schneems)
+    patch :update, id: repo_subscriptions(:schneems_to_triage).id,
+    repo_subscription: { email_limit: 0 }
+    assert_equal flash[:error], "Something went wrong"
+    assert_redirected_to :back
   end
 end
-
