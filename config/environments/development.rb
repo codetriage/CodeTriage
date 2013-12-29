400e56a202f5876d72bdda05a7bdbf3316ea4ec8
Vipul A M <vipulnsward@gmail.com>
2013-12-29 22:20:00 +0530

    CodeTriage is CodeTriage.
    Rename app from `Example` to `CodeTriage`. :panda_face:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Example::Application.configure do
+CodeTriage::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
