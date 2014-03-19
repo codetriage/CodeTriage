2f452477278e2e1b367450591c6c21720b051dc2
Arthur Neves <arthurnn@gmail.com>
2014-03-19 12:59:57 -0400

    Add test for full GitHub URL submit

--- test/integration/adding_repos_test.rb
+++ test/integration/adding_repos_test.rb
@@ -25,6 +25,26 @@ class AddingReposTest < ActionController::IntegrationTest
     end
   end
 
+  test "adding a new valid repo passing the URL" do
+    login_via_github
+    VCR.use_cassette('my_repos') do
+      visit "/"
+      click_link "Submit a Repo"
+
+      fill_in 'url', :with => 'https://github.com/bemurphy/issue_triage_sandbox'
+      fill_in 'repo_name',      :with => ''
+
+      within '#new_repo_from_url' do
+        click_button "Add Repo"
+      end
+    end
+
+    VCR.use_cassette('add_valid_repo') do
+      assert page.has_content?("Added bemurphy/issue_triage_sandbox for triaging")
+    end
+  end
+
+
   # TODO: Pending for now but we should enable this, there's an env change in repo that
   # treats all repo urls as valid in test.  Fix this one that is removed
   # test "adding an invalid repo" do
