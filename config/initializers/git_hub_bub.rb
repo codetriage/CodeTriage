18eb0bfc0f1eb48e1da3c0044978674699c2f76c
schneems <richard.schneeman@gmail.com>
2014-07-15 10:55:27 -0500

    fix auth_is_valid?

--- config/initializers/git_hub_bub.rb
+++ config/initializers/git_hub_bub.rb
@@ -5,7 +5,7 @@ GitHubBub::Request::GITHUB_VERSION = "vnd.github.v3.full+json"
 
 # Auth all non authed requests (due to github request limits)
 GitHubBub::Request.set_before_callback do |request|
-  if request.options.key?(:token) || request.token?
+  if request.token?
     # Request is authorized, do nothing
   else
     request.token = ENV['GITHUB_API_KEY'] || User.random.where.not(token: nil).first.token
