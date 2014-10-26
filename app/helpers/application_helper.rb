2761843b4b0852660e9b6f4e437d9d6ea3a722d3
Rishi Jain <rishi@joshsoftware.com>
2014-10-26 01:04:47 -0500

    Indented code in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def flash_class(level)
-  case level
-  when :notice then 'info'
-  when :error then 'error'
-  when :alert then 'warning'
-  end
+    case level
+    when :notice then 'info'
+    when :error then 'error'
+    when :alert then 'warning'
+    end
   end
 end
