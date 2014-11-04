9c3d299dccebac0299cf40d65498104a633a12c1
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-11-05 01:17:27 +0530

    Updates to configuration files

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
-# Load the rails application
+# Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-
-
-# Initialize the rails application
-CodeTriage::Application.initialize!
-
+# Initialize the Rails application.
+Rails.application.initialize!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-CodeTriage::Application.config.session_store :cookie_store, key: '_example_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# CodeTriage::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_code_triage_session'
