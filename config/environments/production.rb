a895dd8469bedfe7f9b33a62505ac4d163280489
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-11-19 10:06:50 +0530

    Use older production settings for assets
    - They were removed while upgrading to Rails 4.8.
    - https://github.com/codetriage/codetriage/blob/af554ad9d059ccc277f90cc5f51e9442514dc41d/config/environments/production.rb#L11
    - https://github.com/codetriage/codetriage/blob/af554ad9d059ccc277f90cc5f51e9442514dc41d/config/environments/production.rb#L14
    - Added them back.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,10 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
+  config.assets.compress = true
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
