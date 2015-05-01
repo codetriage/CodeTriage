cb817011e14269699364d2541678f61faffaf101
Chris C Cerami <chrisccerami@gmail.com>
2015-04-30 20:48:05 -0400

    Render errors instead of exception when submitting invalid repo params #314

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -37,14 +37,12 @@ class ReposController < RepoBasedController
 
   def create
     parse_params_for_repo_info
-    @repo   = Repo.search_by(params[:repo][:name], params[:repo][:user_name]).first
-    @repo ||= Repo.create!(repo_params)
-
+    @repo   = Repo.search_by(params[:repo][:name], params[:repo][:user_name]).first unless params_blank?
+    @repo ||= Repo.new(repo_params)
     if @repo.save
       flash[:notice] = "Added #{@repo.to_param} for triaging"
       repo_sub = RepoSubscription.create!(repo_id: @repo.id, user_id: current_user.id)
       RepoSubscription.background_send_triage_email(repo_sub.id)
-
       redirect_to @repo
     else
       response = GitHubBub.get("/user/repos", type: "owner", token: current_user.token)
@@ -85,8 +83,12 @@ class ReposController < RepoBasedController
       if params[:url]
         params[:url].match(/^https:\/\/github\.com\/([^\/]*)\/([^\/]*)\/?$/)
         params[:repo] ||= {}
-        params[:repo][:user_name] = $1
-        params[:repo][:name] = $2
+        params[:repo][:user_name] = $1.to_s
+        params[:repo][:name] = $2.to_s
       end
+    end
+
+    def params_blank?
+      repo_params.values.any? { |param| param.blank? }
     end
 end
