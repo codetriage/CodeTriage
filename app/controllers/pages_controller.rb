6f4665e5c4bf6d031b787c375deb322531c17ce7
Jym Paul A. Carandang <jympaul26@gmail.com>
2015-06-17 09:48:21 +0800

    Set 50vh before scrolling appears

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
-    @repos = Repo.repos_needing_help_for_user(current_user)
+    # @repos = Repo.repos_needing_help_for_user(current_user)
+    @repos = Repo.all
     @repos_subs = current_user.repo_subscriptions.page(params[:page]||1).per_page(params[:per_page]||50) if user_signed_in?
   end
 end
