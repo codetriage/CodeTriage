d69d2766ba2f30768b697a717d34297c72752463
Jym Paul A. Carandang <jympaul26@gmail.com>
2015-06-25 10:08:14 +0800

    Change update to update! and created integration test
    Used update! instead of update for setting delivered status of emails and created integration test for both receiving issues on repo subscription and
    on clicking "Send new issue!" button

--- app/models/repo_subscription.rb
+++ app/models/repo_subscription.rb
@@ -20,7 +20,7 @@ class RepoSubscription < ActiveRecord::Base
     IssueAssigner.new(repo_sub.user, [repo_sub]).assign
     if assignment = repo_sub.user.issue_assignments.order(:created_at).eager_load(:repo_subscription)
                             .where(repo_subscriptions: { repo_id: repo_sub.repo_id }).last
-      assignment.update(delivered: true)
+      assignment.update!(delivered: true)
       UserMailer.send_triage(repo: repo_sub.repo, user: repo_sub.user, assignment: assignment).deliver_now
     end
   end
