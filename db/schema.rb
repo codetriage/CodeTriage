37818158d737f205f840c53a5ce712bf25cd401c
Jace Monje <jace@aelogica.com>
2015-06-29 06:07:14 +0800

    removed useless code

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(version: 20140710164307) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "issue_assignments", force: :cascade do |t|
     t.integer  "issue_id"
