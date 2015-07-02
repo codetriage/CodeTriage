cda77a82f2d1103a3cf4c6a7b20cf32289daacfb
Jym Paul A. Carandang <jympaul26@gmail.com>
2015-07-02 12:38:16 +0800

    cleaned test

--- test/unit/language_subscriptions_test.rb
+++ test/unit/language_subscriptions_test.rb
@@ -23,26 +23,6 @@ class LanguageSubscriptionsTest < ActiveSupport::TestCase
     assert_equal 2, user.language_subscriptions.first.issue_assignments.count
   end
 
-  # test 'the assign_issue creates an assignment for the user' do
-  #   user          = users(:schneems)
-  #   lang_sub      = language_subscriptions(:schneems_to_ruby)
-  #
-  # #   issue                 = repo.issues.new
-  # #   issue.title           = "Foo Bar"
-  # #   issue.url             = "http://schneems.com"
-  # #   issue.last_touched_at = 2.days.ago
-  # #   issue.state           = 'open'
-  # #   issue.html_url        = "http://schneems.com"
-  # #   issue.number          = 1
-  # #   issue.save
-  # #
-  # #   VCR.use_cassette('open_issue') do
-  # #     Issue.any_instance.stubs(:valid_for_user?).returns(true)
-  # #     IssueAssigner.new(user, [repo_sub]).assign
-  # #     assert_equal 1, user.issue_assignments.count
-  # #   end
-  # end
-
   test "email_limit allows multiple issues per repo" do
     user          = users(:schneems)
     lang_sub      = language_subscriptions(:schneems_to_ruby)
@@ -51,14 +31,5 @@ class LanguageSubscriptionsTest < ActiveSupport::TestCase
     lang_sub.get_issues
     assert_equal 2, user.language_subscriptions.first.issue_assignments.count
   end
-
-  # test ".subscriptions_for a repo" do
-  #   user          = users(:schneems)
-  #   lang_sub      = language_subscriptions(:schneems_to_ruby)
-  #
-  #   Issue.any_instance.stubs(:valid_for_user?).returns(true)
-  #   lang_sub.get_issues
-  #   assert_equal 2, user.language_subscriptions.first.issue_assignments.count
-  # end
 
 end
