5cd1fa225d45370eb73c2a2c0f49cb4613f63f7d
Kyle Fiedler <kyle@kylefiedler.com>
2015-10-08 13:42:34 -0400

    Remove fade from alert transition

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -5,7 +5,7 @@ jQuery(function() {
 });
 
 window.setTimeout(function() {
-  $(".alert").fadeTo(500, 0).slideUp(500, function(){
+  $(".alert").slideUp(500, function(){
       $(this).remove();
   });
 }, 5000);
