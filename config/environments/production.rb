ce6a615653c76b058186efe750d76fcdd69ed220
schneems <richard.schneeman@gmail.com>
2015-10-15 15:33:26 -0500

    Actually use memcache in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,6 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Use a different cache store in production
-  config.cache_store = :dalli_store
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
@@ -91,4 +89,18 @@ Rails.application.configure do
   config.log_tags = [:uuid]
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  if ENV["MEMCACHIER_SERVERS"]
+    config.cache_store = :dalli_store,
+                        (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                        {:username => ENV["MEMCACHIER_USERNAME"],
+                         :password => ENV["MEMCACHIER_PASSWORD"],
+                         :failover => true,
+                         :socket_timeout => 1.5,
+                         :socket_failure_delay => 0.2
+                        }
+    else
+      config.cache_store = :dalli_store
+    end
 end
