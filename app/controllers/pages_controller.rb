5825b8f8e78f8a8b503b8d586c4e817769b66eb3
schneems <richard.schneeman@gmail.com>
2015-10-16 17:07:48 -0500

    whoops

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @repos = Repo.repos_needing_help_for_user(current_user).limit(10)
+    @repos = Repo.repos_needing_help_for_user(current_user)
     @repos_subs = current_user.repo_subscriptions.page(params[:page]||1).per_page(params[:per_page]||50) if user_signed_in?
   end
 end
