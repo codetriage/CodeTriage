77eaac030e0b33f1dac93c0bb2979270269cd666
schneems <richard.schneeman@gmail.com>
2015-10-19 22:29:21 -0500

    Speed!
    - Use multi_fetch_fragments for fast home page rendering
    - Add pagination so we don't render ALL the repos
    - Avoid needless partial calling
    - Add javascript ajax based language selection, though now that I think about it this probably would have been a perfect use case for turbo links.
    - Speed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,25 @@
 class PagesController < ApplicationController
   def index
-    @repos = Repo.repos_needing_help_for_user(current_user)
-    @repos_subs = current_user.repo_subscriptions.page(params[:page]||1).per_page(params[:per_page]||50) if user_signed_in?
+    @repos = Repo.with_some_issues
+    if language = valid_params[:language] || current_user.try(:favorite_languages)
+      @repos = @repos.where(language: language)
+    end
+    @repos = @repos.order_by_issue_count.page(valid_params[:page]).per_page(valid_params[:per_page] || 52)
+
+    if user_signed_in?
+      @repos_subs = current_user.repo_subscriptions.page(valid_params[:page]).per_page( valid_params[:per_page] || 52).includes(:repo)
+    end
+
+    respond_to do |format|
+      format.html { }
+      format.json do
+        htmlForPage = render_to_string(partial: "repos_with_pagination", locals: {repos: @repos}, formats: ['html'])
+        render json: { html: htmlForPage }.to_json
+      end
+    end
+  end
+
+  def valid_params
+    params.permit(:language, :per_page, :page)
   end
 end
