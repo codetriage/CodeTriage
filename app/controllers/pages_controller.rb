f22017c6737be08bfc7e83c9df95e0e06055c024
schneems <richard.schneeman@gmail.com>
2015-10-21 14:30:08 -0500

    50 per page, why not.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,10 +4,10 @@ class PagesController < ApplicationController
     if language = valid_params[:language] || current_user.try(:favorite_languages)
       @repos = @repos.where(language: language)
     end
-    @repos = @repos.order_by_issue_count.page(valid_params[:page]).per_page(valid_params[:per_page] || 52)
+    @repos = @repos.order_by_issue_count.page(valid_params[:page]).per_page(valid_params[:per_page] || 50)
 
     if user_signed_in?
-      @repos_subs = current_user.repo_subscriptions.page(valid_params[:page]).per_page( valid_params[:per_page] || 52).includes(:repo)
+      @repos_subs = current_user.repo_subscriptions.page(valid_params[:page]).per_page( valid_params[:per_page] || 50).includes(:repo)
     end
 
     respond_to do |format|
