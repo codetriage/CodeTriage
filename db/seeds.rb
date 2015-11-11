0c59ce9146f832df27f983f2f5ff4d04387663cb
schneems <richard.schneeman@gmail.com>
2015-11-11 14:42:11 -0600

    Enable Heroku review apps

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.new(
+  github:     "schneems",
+  email:      "foo@example.com",
+  avatar_url: "https://avatars.githubusercontent.com/u/59744?v=3"
+)
+user.save(validate: false)
+repo = user.repos.new(
+  :user_name     => "schneems",
+  :name          => "wicked",
+  :issues_count  => 30,
+  :language      => "Ruby",
+  :description   => "A library or something",
+  :full_name     => "schneems/wicked"
+)
+repo.save(validate: false)
+
+repo.subscribers << user
+
+100.times do |i|
+  issue = repo.issues.new(
+    number:     i,
+    updated_at: Time.now,
+    title:      "H3y did you know about #{i}",
+    state:      "open",
+    html_url:   "https://github.com/schneems/wicked/issues/#{i}"
+  )
+  issue.save
+end
