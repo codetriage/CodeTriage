1b025c265731dd5d8bc705cda0b3c4934e8a7663
schneems <richard.schneeman@gmail.com>
2015-11-10 22:34:35 -0600

    Add a modal to the badge that gives url for easy copy

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+})
