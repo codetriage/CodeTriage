f0c14f04003f732b699246e7a23b7b986d664ca0
Prathamesh Sonpatki <csonpatki@gmail.com>
2016-01-26 07:01:59 +0530

    Configuration file updates

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,3 +2,5 @@
 
 # This is a new Rails 5.0 default, so introduced as a config to ensure apps made with earlier versions of Rails aren't affected when upgrading.
 Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+# Specify a serializer for the signed and encrypted cookie jars.
+# Valid options are :json, :marshal, and :hybrid.
