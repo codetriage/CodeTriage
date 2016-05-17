1ee4ba79ca06c4daf3eea84a85f3748d3829808d
mbie <embielec@gmail.com>
2016-05-17 23:03:48 +0200

    Add origin path to authorize path helper

--- app/helpers/repos_helper.rb
+++ app/helpers/repos_helper.rb
@@ -1,6 +1,6 @@
 module ReposHelper
   def wanna_triage_button(repo, html_class: "")
-    path = user_signed_in? ? repo_subscriptions_path(repo_id: repo.id) : user_omniauth_authorize_path(:github)
+    path = user_signed_in? ? repo_subscriptions_path(repo_id: repo.id) : user_omniauth_authorize_path(:github, origin: request.fullpath)
     button_to "I Want to Triage #{repo.path}", path, class: "button #{html_class}"
   end
 end
