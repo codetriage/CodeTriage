cc0a223f00d5b6afe2705fb810312e4e0f0a2ac8
schneems <richard.schneeman@gmail.com>
2016-06-22 16:56:18 -0500

    Avoid all the warnings we can
    There is still a warning from scout_apm telling us that things are working fine. I opened an issue https://github.com/scoutapp/scout_apm_ruby/issues/49

--- config/initializers/git_hub_bub.rb
+++ config/initializers/git_hub_bub.rb
@@ -1,7 +1,11 @@
+# I know it's awful but I don't have time to fix the GitHubBub API and this makes `rails c` not noisey
+GitHubBub::Request.send(:remove_const, :GITHUB_VERSION)
+GitHubBub::Request.send(:remove_const, :USER_AGENT)
+GitHubBub::Request.send(:remove_const, :RETRIES)
 
-GitHubBub::Request::USER_AGENT = 'codetriage'
-GitHubBub::Request::RETRIES    = 3
-GitHubBub::Request::GITHUB_VERSION = "vnd.github.v3.full+json"
+GitHubBub::Request::GITHUB_VERSION = 'vnd.github.v3.full+json'
+GitHubBub::Request::USER_AGENT     ='codetriage'
+GitHubBub::Request::RETRIES        = 3
 
 # Auth all non authed requests (due to github request limits)
 GitHubBub::Request.set_before_callback do |request|
