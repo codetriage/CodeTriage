d94560d165d45f8efb19d30ab38cb253114db8ae
Kristofer Rye <kristofer.rye@gmail.com>
2016-07-25 13:41:43 -0500

    RepoBasedController: Fix whitespace

--- app/controllers/repo_based_controller.rb
+++ app/controllers/repo_based_controller.rb
@@ -1,11 +1,11 @@
 class RepoBasedController < ApplicationController
-  protected
+	protected
 
-    def name_from_params(options)
-      [options[:name], options[:format]].compact.join('.')
-    end
+	def name_from_params(options)
+		[options[:name], options[:format]].compact.join('.')
+	end
 
-    def find_repo(options)
-      Repo.find_by_full_name(options[:full_name])
-    end
+	def find_repo(options)
+		Repo.find_by_full_name(options[:full_name])
+	end
 end
