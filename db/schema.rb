d8ada74b7a7ad8b9a3654a73bc5f325ad3d79742
Ganesh Sagare <ganesh@joshsoftware.com>
2016-08-27 08:19:48 +0530

    Added Stars Count Feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613220116) do
+ActiveRecord::Schema.define(version: 20160827023209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(version: 20160613220116) do
     t.datetime "updated_at"
     t.text     "github_error_msg"
     t.string   "commit_sha"
+    t.integer  "stars_count",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
