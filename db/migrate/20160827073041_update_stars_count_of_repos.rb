bed47a8f307729f30783f96d6247c0918cc0e763
Ganesh Sagare <ganesh@joshsoftware.com>
2016-08-27 13:49:30 +0530

    1. Added Migration to update Repos stars count. 2. Fixed Alignment.

---
+++ db/migrate/20160827073041_update_stars_count_of_repos.rb
@@ -0,0 +1,10 @@
+class UpdateStarsCountOfRepos < ActiveRecord::Migration[5.0]
+  def change
+    Repo.find_each do |repo|
+      repo_path = File.join "repos", "#{repo.user_name}/#{repo.name}"
+      resp = GitHubBub.get(repo_path).json_body
+      repo.stars_count = resp["stargazers_count"]
+      repo.save!
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160827023209) do
+ActiveRecord::Schema.define(version: 20160827073041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
