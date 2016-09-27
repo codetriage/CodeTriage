689c6e1fae93fa5417a5e3cbc6e17d56373973c7
schneems <richard.schneeman@gmail.com>
2016-09-27 15:00:32 -0500

    Fix db migrate logic

--- db/migrate/20160827073041_update_stars_count_of_repos.rb
+++ db/migrate/20160827073041_update_stars_count_of_repos.rb
@@ -1,6 +1,6 @@
 class UpdateStarsCountOfRepos < ActiveRecord::Migration[5.0]
   def change
-    Repo.where.not(stars_count: nil).find_each do |repo|
+    Repo.where(stars_count: nil).find_each do |repo|
       repo_path = File.join "repos", "#{repo.user_name}/#{repo.name}"
       resp = GitHubBub.get(repo_path).json_body
       repo.stars_count = resp["stargazers_count"]
