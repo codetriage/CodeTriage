087a2291f982d3ade3ddb90ae0b526931fe1c0eb
schneems <richard.schneeman@gmail.com>
2016-09-27 14:48:48 -0500

    This has been breaking release phase since september

--- db/migrate/20160827073041_update_stars_count_of_repos.rb
+++ db/migrate/20160827073041_update_stars_count_of_repos.rb
@@ -1,6 +1,6 @@
 class UpdateStarsCountOfRepos < ActiveRecord::Migration[5.0]
   def change
-    Repo.find_each do |repo|
+    Repo.where.not(stars_count: nil).find_each do |repo|
       repo_path = File.join "repos", "#{repo.user_name}/#{repo.name}"
       resp = GitHubBub.get(repo_path).json_body
       repo.stars_count = resp["stargazers_count"]
