706b24be286904eacc72544a58ca53f8b706e5ef
Nate Berkopec <nate.berkopec@gmail.com>
2017-07-04 07:04:27 -0600

    User#edit must have current_user. Fixes CODETRIAGE-7B

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!, only: [:update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])
