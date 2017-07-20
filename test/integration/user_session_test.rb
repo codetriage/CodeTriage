2d04ef196ca5f6cbbec83d8d9653d66a55d8e678
Wouter de Vos <wouter@codaisseur.com>
2017-07-20 23:44:39 +0200

    Remove :registerable from User devise modules Fixes codetriage/codetriage#580

---
+++ test/integration/user_session_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class UserSessionTest < ActionDispatch::IntegrationTest
+  test "sign_in screen renders" do
+    visit new_user_session_path
+    assert page.has_content?('Sign in')
+  end
+end
