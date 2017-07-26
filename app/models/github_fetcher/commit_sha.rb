613d1729915eb97c90c31e08687793f279832a8f
Nate Berkopec <nate.berkopec@gmail.com>
2017-07-26 12:32:55 -0600

    I forgot how default arguments work. Fix CODETRIAGE-C3

--- app/models/github_fetcher/commit_sha.rb
+++ app/models/github_fetcher/commit_sha.rb
@@ -1,12 +1,12 @@
 module GithubFetcher
   class CommitSha < Resource
-    def initialize(user_name:, name:, default_branch: "master")
+    def initialize(user_name:, name:, default_branch:)
       @api_path = File.join(
         'repos',
         user_name,
         name,
         'commits',
-        default_branch
+        default_branch || "master"
       )
       super({})
     end
