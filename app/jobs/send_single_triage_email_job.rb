cc15a9cf32c27469f7ad6f146644f3ed4c6cd58e
Benoit Tigeot <btigeot@octo.com>
2017-08-11 17:17:37 +0200

    Use private method for assignment definition

--- app/jobs/send_single_triage_email_job.rb
+++ app/jobs/send_single_triage_email_job.rb
@@ -2,10 +2,16 @@ class SendSingleTriageEmailJob < ApplicationJob
   def perform(id)
     repo_sub = RepoSubscription.includes(:user, :repo).find(id)
     IssueAssigner.new(repo_sub.user, [repo_sub]).assign!
-    if assignment = repo_sub.user.issue_assignments.order(:created_at).eager_load(:repo_subscription)
-                            .where(repo_subscriptions: { repo_id: repo_sub.repo_id }).last
+    if assignment
       assignment.update!(delivered: true)
       UserMailer.send_triage(repo: repo_sub.repo, user: repo_sub.user, assignment: assignment).deliver_later
     end
+  end
+
+  private
+
+  def assignment
+    @assignment ||= repo_sub.user.issue_assignments.order(:created_at).eager_load(:repo_subscription)
+                            .where(repo_subscriptions: { repo_id: repo_sub.repo_id }).last
   end
 end
