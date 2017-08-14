806f6312f22f628997cafaa20914752f757e17ca
Nate Berkopec <nate.berkopec@gmail.com>
2017-08-14 11:08:22 -0600

    Fix missing repo_sub in assignment. Fix CODETRIAGE-DR

--- app/jobs/send_single_triage_email_job.rb
+++ app/jobs/send_single_triage_email_job.rb
@@ -2,7 +2,7 @@ class SendSingleTriageEmailJob < ApplicationJob
   def perform(id)
     repo_sub = RepoSubscription.includes(:user, :repo).find(id)
     IssueAssigner.new(repo_sub.user, [repo_sub]).assign!
-    if assignment
+    if assignment(repo_sub)
       assignment.update!(delivered: true)
       UserMailer.send_triage(repo: repo_sub.repo, user: repo_sub.user, assignment: assignment).deliver_later
     end
@@ -10,7 +10,7 @@ class SendSingleTriageEmailJob < ApplicationJob
 
   private
 
-  def assignment
+  def assignment(repo_sub)
     @assignment ||= repo_sub.user.issue_assignments.order(:created_at).eager_load(:repo_subscription)
                             .where(repo_subscriptions: { repo_id: repo_sub.repo_id }).last
   end
