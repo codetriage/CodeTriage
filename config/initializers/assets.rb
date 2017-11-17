576163945e13c1dc06c935c64a2d9a145c473f83
schneems <richard.schneeman@gmail.com>
2017-11-17 10:08:00 -0600

    Minify assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.css_compressor = :sass
+Rails.application.config.assets.js_compressor = :uglifier
