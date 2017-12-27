ad0f76298e4b5344a940efd83cda2b0aa788ebab
Prathamesh Sonpatki <csonpatki@gmail.com>
2017-12-27 21:03:09 +0530

    secret.secret_token is deprecated
    Ref: https://github.com/rails/rails/commit/fbcc4bfe9a211e219da5d0bb01d894fcdaef0a0e

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CodeTriage::Application.config.secret_token    = ENV['SECRET_TOKEN']    || 'alskdjfliasuofiu1o23f9c8aucm98pjoiuxhp19y2o37yf98fynogvan87xydfi8ampsiodfhmxpo'
 CodeTriage::Application.config.secret_key_base = ENV['SECRET_KEY_BASE'] || 'ab37b1294efd8458ee5f186d1164e91bb234e123b8244fbc009f50b223a655f2b4265e519b93db715d9b3c8a382b7fe961ef9b2857105f14efaf32ce72f52e9e'
