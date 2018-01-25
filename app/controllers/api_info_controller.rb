ca80ece0baf10aeaeca3b11b8f04175abd85a375
schneems <richard.schneeman@gmail.com>
2018-01-24 22:08:36 -0600

    Fix rubocop

--- app/controllers/api_info_controller.rb
+++ app/controllers/api_info_controller.rb
@@ -8,10 +8,12 @@ class ApiInfoController < RepoBasedController
     added_by = @repo.subscribers.where(private: false).order(:created_at).first&.github
     respond_to do |format|
       format.json do
-        render json: { subscriber_count:  @repo.subscribers_count,
-                       added_by_username: added_by,
-                       time_ago:          time_ago_in_words(@repo.created_at),
-                       message:           "This is an unstable interface, do not use" }.to_json
+        render json: {
+          subscriber_count:  @repo.subscribers_count,
+          added_by_username: added_by,
+          time_ago:          time_ago_in_words(@repo.created_at),
+          message:           "This is an unstable interface, do not use"
+        }.to_json
       end
     end
   end
