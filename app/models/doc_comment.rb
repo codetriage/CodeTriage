c61628c36e5509f1ed47572f0dd0f4df548fdab9
schneems <richard.schneeman+foo@gmail.com>
2018-02-08 12:23:00 -0600

    WIPWIP

--- app/models/doc_comment.rb
+++ app/models/doc_comment.rb
@@ -9,6 +9,20 @@ class DocComment < ActiveRecord::Base
     doc_method_id.present?
   end
 
+  def md_safe_comment_block
+    md = comment
+    if md.match?(/^~~~|^```/)
+      # If a comment starts with a code fence i.e. ~~~ or ```
+      # Then indent the whole thing by one character
+      # makes comments safe to use in markdown without
+      # breaking out of code blocks
+      md.gsub!(/^(.)/, ' \1')
+    end
+    md = DocMethod::NeedsDocs if md.blank?
+
+    return "~~~\n#{md}\n~~~".html_safe
+  end
+
   def doc_class?
     doc_class_id.present?
   end
