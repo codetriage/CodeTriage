659b8be64262c18dd560fc890f185399f25c7480
schneems <richard.schneeman+foo@gmail.com>
2018-03-04 21:15:05 -0600

    If no public subscribers show zero count

--- app/controllers/api_info_controller.rb
+++ app/controllers/api_info_controller.rb
@@ -6,10 +6,13 @@ class ApiInfoController < RepoBasedController
   def show
     @repo    = find_repo(params)
     added_by = @repo.subscribers.where(private: false).order(:created_at).first&.github
+    count = @repo.subscribers_count
+    count = 0 if added_by.nil?
+
     respond_to do |format|
       format.json do
         render json: {
-          subscriber_count:  @repo.subscribers_count,
+          subscriber_count:  count,
           added_by_username: added_by,
           time_ago:          time_ago_in_words(@repo.created_at),
           message:           "This is an unstable interface, do not use"
