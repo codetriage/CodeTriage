057486127773091878dd254b6e69512da9be61d0
schneems <richard.schneeman+foo@gmail.com>
2018-03-07 10:32:10 -0600

    Fix rubocop

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-
 if !ENV['NO_BOOTSNAP']
   require 'bootsnap'
   Bootsnap.setup(
