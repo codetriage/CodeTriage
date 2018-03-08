80d59069b8356b2a134c637dc633889b9eb7810e
schneems <richard.schneeman+foo@gmail.com>
2018-03-08 16:56:05 -0600

    Test mailer thanks to @chrisarcand

---
+++ test/emails/send_daily_triage_email_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  include ActiveJob::TestHelper::TestQueueAdapter
+
+  test "sends issues" do
+    user = users(:schneems)
+
+    assert_performed_with(job: ActionMailer::DeliveryJob, queue: "mailers") do
+      SendDailyTriageEmailJob.new.perform(user)
+    end
+
+    triage_email = ActionMailer::Base.deliveries.last
+
+    assert_match /Help Triage 2 Open Source Issues/, triage_email.subject
+
+    triage_email_text =  triage_email.text_part.to_s
+
+    # Repo groups
+    assert_match /## rails\/rails/,                   triage_email_text
+    assert_match /## bemurphy\/issue_triage_sandbox/, triage_email_text
+
+    # Issue number one
+    assert_match /rails\/rails#1/,                    triage_email_text
+  end
+
+  test "sends docs" do
+    user = users(:bar_user)
+
+    assert_performed_with(job: ActionMailer::DeliveryJob, queue: "mailers") do
+      SendDailyTriageEmailJob.new.perform(user)
+    end
+    triage_email = ActionMailer::Base.deliveries.last
+    triage_email_text =  triage_email.text_part.to_s
+
+    assert_match /### Docs/, triage_email_text
+  end
+end
