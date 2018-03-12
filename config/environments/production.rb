6256c84baa77eed074426b29b19537361355d743
schneems <richard.schneeman+foo@gmail.com>
2018-03-12 14:11:18 -0500

    Not sure why this is needed
    Images aren’t using a full url in emails.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.asset_host          = "https://www.codetriage.com"
   config.action_mailer.default_url_options = { host: "codetriage.com" }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
