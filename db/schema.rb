36d4196eec84aca2d9cdd00d7501a42c7429a7f1
Prathamesh Sonpatki <csonpatki@gmail.com>
2018-06-29 15:22:10 +0530

    Upggrade Ruby

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171125212307) do
+ActiveRecord::Schema.define(version: 2017_11_25_212307) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
   enable_extension "pg_stat_statements"
+  enable_extension "plpgsql"
 
   create_table "data_dumps", id: :serial, force: :cascade do |t|
     t.text "data"
