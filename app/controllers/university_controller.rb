0a7ce8f80671298a8edf864af9c6d814cc552eed
schneems <richard.schneeman+foo@gmail.com>
2018-07-30 13:17:52 -0500

    Render example app page as markdown

--- app/controllers/university_controller.rb
+++ app/controllers/university_controller.rb
@@ -1,9 +1,12 @@
 class UniversityController < ApplicationController
+  layout "markdown_page"
+
   def show
     case params[:id]
     when 'example_app', 'example_apps', 'example-app', 'example-apps'
-      set_title("Please provide an example app")
+      set_title("Please Provide an Example App")
       set_description("Get answers for your open source bug reports faster by providing an example app. Find out how.")
+      @page_title = "Plsease Provide an Example App"
       render 'example_app'
     end
   end
