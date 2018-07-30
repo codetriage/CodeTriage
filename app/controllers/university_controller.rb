c8e9cc4648741b35ca57564068463de908b3cd94
schneems <richard.schneeman+foo@gmail.com>
2018-07-30 13:40:53 -0500

    Fix typo

--- app/controllers/university_controller.rb
+++ app/controllers/university_controller.rb
@@ -6,7 +6,7 @@ class UniversityController < ApplicationController
     when 'example_app', 'example_apps', 'example-app', 'example-apps'
       set_title("Please Provide an Example App")
       set_description("Get answers for your open source bug reports faster by providing an example app. Find out how.")
-      @page_title = "Plsease Provide an Example App"
+      @page_title = "Please Provide an Example App"
       render 'example_app'
     end
   end
