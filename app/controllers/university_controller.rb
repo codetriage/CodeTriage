82a747af2e650fc5e0b4137b5de67dd3788f019f
schneems <richard.schneeman+foo@gmail.com>
2018-07-30 20:17:14 -0500

    Picking the Right Repo(s) university page

--- app/controllers/university_controller.rb
+++ app/controllers/university_controller.rb
@@ -8,6 +8,11 @@ class UniversityController < ApplicationController
       set_description("Get answers for your open source bug reports faster by providing an example app. Find out how.")
       @page_title = "Please Provide an Example App"
       render 'example_app'
+    when 'picking_a_repo'
+      set_title("Picking the Right Repo(s) to start your Open Source Contribution Journey")
+      set_description("One of the most important decisions you'll make when you start contributing is which open source libraries you contribute to")
+      @page_title = "Picking the Right Repo(s)"
+      render 'picking_a_repo'
     end
   end
 end
