d4b49fceb307aff6160c709972855a7a772af69d
schneems <richard.schneeman+foo@gmail.com>
2018-09-21 12:45:20 -0500

    Clear cache on every load DO NOT PUT INTO PRODUCTION DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION
    DO NOT PUT INTO PRODUCTION

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+Rails.cache.clear
