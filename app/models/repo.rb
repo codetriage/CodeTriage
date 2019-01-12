8aca59d605f7079c1bed2f3e105dd299dbe5d80f
Jason Hsu <jhsu802701@jasonhsu.com>
2019-01-12 22:07:59 +0000

    subscriber_count changed to subscribers_count; added integration test for displaying public users while concealing private users

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -106,7 +106,7 @@ class Repo < ActiveRecord::Base
     end
   end
 
-  def subscriber_count
+  def subscribers_count
     users.count
   end
 
---
+++ test/integration/user_private_test.rb
@@ -0,0 +1,66 @@
+require 'test_helper'
+
+class UserPrivateTest < ActionDispatch::IntegrationTest
+  def add_subscription(new_user)
+    @repo = repos(:issue_triage_sandbox)
+    @repo_sub_new_user = new_user.repo_subscriptions.new
+    @repo_sub_new_user.repo = @repo
+    @repo_sub_new_user.save
+  end
+
+  def load_subscriptions
+    @user_mockstar = users(:mockstar)
+    @user_schneems = users(:schneems)
+    @user_schneems.avatar_url = 'http://gravatar.com/avatar/schneems'
+    @user_schneems.save
+    @user_foo = users(:foo_user)
+    @user_foo.avatar_url = 'http://gravatar.com/avatar/foo'
+    @user_foo.save
+    @user_bar = users(:bar_user)
+    @user_bar.avatar_url = 'http://gravatar.com/avatar/bar'
+    @user_bar.private = true
+    @user_bar.save
+    @user_jroes = users(:jroes)
+    @user_jroes.avatar_url = 'http://gravatar.com/avatar/jroes'
+    @user_jroes.save
+    @user_empty = users(:empty)
+    @user_empty.avatar_url = 'http://gravatar.com/avatar/empty'
+    @user_empty.save
+    add_subscription(@user_jroes)
+    add_subscription(@user_empty)
+  end
+
+  def xpath_img(url_avatar)
+    str1 = './/img[@src="'
+    str2 = url_avatar
+    str3 = '"]'
+    output = "#{str1}#{str2}#{str3}"
+    output
+  end
+
+  def check_public_user(user1)
+    page.assert_selector(:xpath, xpath_img(user1.avatar_url))
+    assert page.has_link?(href: user1.github_url)
+  end
+
+  def check_private_user(user1)
+    assert_not page.has_link?(href: user1.github_url)
+  end
+
+  test 'display only public users' do
+    load_subscriptions
+    login_as(@user_mockstar, scope: :user)
+    visit root_path
+    click_on 'bemurphy/issue_triage_sandbox'
+    check_public_user(@user_schneems)
+    check_public_user(@user_foo)
+    check_private_user(@user_bar)
+    check_private_user(@user_jroes)
+  end
+
+  test 'public user can become private' do
+  end
+
+  test 'private user can become public' do
+  end
+end
