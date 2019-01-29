78e1d593207913bf40ba46ced7bc7e6149262d17
Bruno Martins <brunomart.91@gmail.com>
2019-01-29 23:02:04 +0000

    Remove the endpoints generated by 'resources' without a matching controller method

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ CodeTriage::Application.routes.draw do
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
-  resources :doc_methods
+  resources :doc_methods, only: [:show]
 
-  resources :university
+  resources :university, only: [:show]
   get 'example_app' => 'university#show', id: 'example_app'
 
   devise_for :users, skip: [:registration],
@@ -27,27 +27,27 @@ CodeTriage::Application.routes.draw do
   get 'support' => 'pages#support'
 
   namespace :users do
-    resources :after_signup
+    resources :after_signup, only: [:show, :update]
   end
 
-  resources   :users, except: [:new, :create, :index]
+  resources   :users, only: [:show, :edit, :update, :destroy]
   get         "/users/unsubscribe/:account_delete_token" => "users#token_delete", as: :token_delete_user
   delete      "/users/unsubscribe/:account_delete_token" => "users#token_destroy"
 
-  resources   :issue_assignments
+  resources   :issue_assignments, only: [:create]
 
   get "/issue_assignments/:id/users/:user_id/click",  to: "issue_assignments#click_issue_redirect", as: :issue_click
   get "/doc_methods/:id/users/:user_id/click",        to: "doc_methods#click_method_redirect",      as: :doc_method_click
   get "/doc_methods/:id/users/:user_id/source_click", to: "doc_methods#click_source_redirect",      as: :doc_source_click
 
-  resources :repo_subscriptions
+  resources :repo_subscriptions, only: [:create, :destroy, :update]
 
   get 'mail_view', to: redirect('rails/mailers')
 
   # format: false gives us rails 3.0 style routes so angular/angular.js is interpreted as
   # user_name: "angular", name: "angular.js" instead of using the "js" as a format
   scope format: false do
-    resources :repos, only: %w[index new create] do
+    resources :repos, only: [:index, :new, :create] do
       collection do
         get :list
       end
