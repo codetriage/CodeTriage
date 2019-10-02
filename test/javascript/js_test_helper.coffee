f0e57d8b36f3e868b98caa8d034258c4b3c5bcf9
schneems <richard.schneeman+foo@gmail.com>
2019-10-02 16:48:01 -0500

    Remove teaspoon support for now

--- test/javascript/js_test_helper.coffee
+++
@@ -1,4 +0,0 @@
-#= require application
-#=require support/sinon
-#=require support/chai
-#=require support/javascript_helpers.js
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -62,33 +62,6 @@ VCR.configure do |c|
   end
 end
 
-# Provide default configuration.
-#
-# You can override various configuration directives defined here by using arguments with the teaspoon command.
-#
-# teaspoon --driver=selenium --suppress-log
-# rake teaspoon DRIVER=selenium SUPPRESS_LOG=false
-
-Teaspoon.setup do |config|
-  # Driver / Server
-  # config.driver           = "phantomjs" # available: phantomjs, selenium
-  # config.server           = nil # defaults to Rack::Server
-
-  # Behaviors
-  # config.server_timeout   = 20 # timeout for starting the server
-  # config.server_port      = nil # defaults to any open port unless specified
-  # config.fail_fast        = true # abort after the first failing suite
-
-  # Output
-  # config.formatters       = "dot" # available: dot, tap, tap_y, swayze_or_oprah
-  # config.suppress_log     = false # suppress logs coming from console[log/error/debug]
-  # config.color            = true
-
-  # Coverage (requires istanbul -- https://github.com/gotwarlost/istanbul)
-  # config.coverage         = true
-  # config.coverage_reports = "text,html,cobertura"
-end
-
 module ActionDispatch
   class IntegrationTest
     include Capybara::DSL
