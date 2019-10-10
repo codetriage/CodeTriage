5f379e8691170947363fb3bffad0c3fb92614c0c
schneems <richard.schneeman+foo@gmail.com>
2019-10-09 20:39:14 -0500

    Fix warning
    https://stackoverflow.com/a/57435700/147390

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module CodeTriage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
