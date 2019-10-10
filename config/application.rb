46cf331c7ad93e6190b777b2b4d280d8f87d3641
Richard Schneeman <richard.schneeman+foo@gmail.com>
2019-10-10 12:04:15 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module CodeTriage
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 5.2 # TODO change to 6.0 and figure out why tests are failing
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
