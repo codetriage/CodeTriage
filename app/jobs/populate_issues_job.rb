19307aad9dec186b70ccc3b3c01252c939011a42
schneems <richard.schneeman+foo@gmail.com>
2019-10-16 14:51:37 -0500

    Rescue and re-raise for more info

--- app/jobs/populate_issues_job.rb
+++ app/jobs/populate_issues_job.rb
@@ -36,7 +36,12 @@ class PopulateIssuesJob < ApplicationJob
     else
       issue_number_to_github_hash = {}
       fetcher.as_json.each do |github_issue_hash|
-        issue_number = github_issue_hash['number']
+        begin
+          issue_number = github_issue_hash['number']
+        rescue TypeError
+          raise "Type error #{github_issue_hash.inspect}, status=#{fetcher.response.status}, body=#{fetcher.response.body}"
+        end
+
         issue_number_to_github_hash[issue_number] = github_issue_hash
       end
 
