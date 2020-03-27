0ec9fc9e8cc0f78b7aaf29c2119321d8a7f3df31
schneems <richard.schneeman+foo@gmail.com>
2020-03-27 11:52:55 -0500

    University index page

--- app/controllers/university_controller.rb
+++ app/controllers/university_controller.rb
@@ -3,6 +3,10 @@
 class UniversityController < ApplicationController
   layout "markdown_page"
 
+  def index
+    @page_title = "Open Source Contribution University"
+  end
+
   def show
     case params[:id]
     when "squash"
@@ -21,14 +25,14 @@ class UniversityController < ApplicationController
       set_description("Get answers for your open source bug reports faster by providing a code that reproduces your problem. Find out how.")
       render 'reproduction_code'
     when 'example_app', 'example_apps', 'example-app', 'example-apps'
-      set_title("Please Provide an Example App")
-      set_description("Get answers for your open source bug reports faster by providing an example app. Find out how right here.")
       @page_title = "Please Provide an Example App"
+      set_title(@page_title)
+      set_description("Get answers for your open source bug reports faster by providing an example app. Find out how right here.")
       render 'example_app'
     when 'picking_a_repo'
+      @page_title = "Picking the Right Repo(s)"
       set_title("Picking the Right Repo(s) to start your Open Source Contribution Journey")
       set_description("One of the most important decisions you'll make when you start contributing is which open source libraries you contribute to. This guide will help you get started")
-      @page_title = "Picking the Right Repo(s)"
       render 'picking_a_repo'
     end
   end
