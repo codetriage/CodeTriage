f88403936c1833971666d790084ab274ee4acf24
schneems <richard.schneeman+foo@gmail.com>
2020-03-26 21:33:31 -0500

    Rebase

--- app/controllers/university_controller.rb
+++ app/controllers/university_controller.rb
@@ -5,14 +5,19 @@ class UniversityController < ApplicationController
 
   def show
     case params[:id]
+    when "rebase"
+      @page_title = "Please Rebase your commits"
+      set_title(@page_title)
+      set_description("What on earth does rebasing commits mean and how do you do it? Find out the what, why, and how of git rebases here.")
+      render 'rebase'
     when "reproduction_code", "reproduction", "reproduction_case"
       @page_title = "Please Provide Reproduction Code"
       set_title(@page_title)
-      set_description("Get answers for your open source bug reports faster by providing an code that reproduces your problem. Find out how.")
+      set_description("Get answers for your open source bug reports faster by providing a code that reproduces your problem. Find out how.")
       render 'reproduction_code'
     when 'example_app', 'example_apps', 'example-app', 'example-apps'
       set_title("Please Provide an Example App")
-      set_description("Get answers for your open source bug reports faster by providing an example app. Find out how.")
+      set_description("Get answers for your open source bug reports faster by providing an example app. Find out how right here.")
       @page_title = "Please Provide an Example App"
       render 'example_app'
     when 'picking_a_repo'
