12001f0adc6fdd9147c1fba3bb50b14455b11cab
schneems <richard.schneeman+foo@gmail.com>
2020-08-30 14:16:28 -0500

    Remove COVID homepage
    The number of COVID related projects Is relatively small and I don't think having them here is particularly driving engagement with those projects.
    
    At the same time, I'm seeing that the overall CodeTriage user growth during the pandemic isn't the same as it was before (eyeballing, I could be wrong). 
    
    I'm still open to exposing these repos somehow, but I think people who don't know what CodeTriage is are getting the mistaken impression it's ONLY covid related, which is not the case.
    
    Let's revert back to the old homepage for now

--- test/integration/filtering_language_test.rb
+++ test/integration/filtering_language_test.rb
@@ -7,17 +7,9 @@ class FilteringLanguageTest < ActionDispatch::IntegrationTest
     Rails.cache.clear
   end
 
-  test "filtering language in covid projects" do
-    visit "/?language_covid=Ruby"
-
-    assert_equal page.all('.types-filter-button')[0].text, 'Ruby'
-    assert_equal page.all('.types-filter-button')[1].text, 'Select Language'
-  end
-
   test "filtering language in normal projects" do
     visit "/?language=Ruby"
 
-    assert_equal page.all('.types-filter-button')[0].text, 'Select Language'
-    assert_equal page.all('.types-filter-button')[1].text, 'Ruby'
+    assert_equal page.all('.types-filter-button')[0].text, 'Ruby'
   end
 end
