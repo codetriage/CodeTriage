f235366c0ae769975e6d2ebff4aed8ac5b4a106b
Nate Berkopec <nate.berkopec@gmail.com>
2020-09-29 18:00:18 -0600

    Puma 5

--- config/puma.rb
+++ config/puma.rb
@@ -13,29 +13,6 @@ threads threads_count, threads_count
 #
 port        ENV.fetch("PORT") { 3000 }
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-preload_app!
-
-nakayoshi_fork true
-wait_for_less_busy_worker
-fork_worker
-
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
