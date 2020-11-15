9b450ee2f2b2f78eb1c762e9ffa14a54e79d595e
Prathamesh Sonpatki <csonpatki@gmail.com>
2020-11-15 18:05:23 +0530

    Config file changes due to Rails 6.1.0.rc1 update

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails/all'
-
-# require 'statsd'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
