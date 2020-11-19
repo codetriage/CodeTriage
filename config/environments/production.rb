fe180f5721846d1b5016b4aa3532166ba83b51f4
schneems <richard.schneeman+foo@gmail.com>
2020-11-19 11:28:41 -0600

    Require active support time in all environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
