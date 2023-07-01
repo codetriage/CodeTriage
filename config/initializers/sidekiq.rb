2c0a5af3f1a2c3124a646fd3f4a3487ec185908e
schneems <richard.schneeman+foo@gmail.com>
2023-07-01 17:59:23 -0400

    Ignore self signed Redis certs
    Heroku upgraded Redis instances to Redis 7. Premium redis instances now enforce TLS (SSL), which is a good thing, however the certs are self-signed which causes a validation error.
    
    The documented fix is to disable the validation. https://devcenter.heroku.com/articles/connecting-heroku-redis#connecting-from-sidekiq
    
    Close #1787

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,19 @@
 
 unless Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV["REDIS_URL"], namespace: "codetriage-sidekiq" }
+    config.redis = {
+      url: ENV["REDIS_URL"],
+      namespace: "codetriage-sidekiq",
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDIS_URL"], namespace: "codetriage-sidekiq" }
+    config.redis = {
+      url: ENV["REDIS_URL"],
+      namespace: "codetriage-sidekiq",
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    }
   end
 end
 
