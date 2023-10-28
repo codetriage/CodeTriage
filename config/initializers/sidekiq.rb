b9db14693da8c8ec91a84c20eb231d897a5cbc28
schneems <richard.schneeman+foo@gmail.com>
2023-10-28 11:48:16 -0500

    Update sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
 
-sidekiq_config = {
+Sidekiq.default_configuration.redis = {
   url: ENV["REDIS_URL"],
-  ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
+  ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_NONE
+  }
 }
-
-Sidekiq.configure_server do |config|
-  config.redis = sidekiq_config
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = sidekiq_config
-end
-
-if Sidekiq.server?
-  Rails.application.config.active_record.warn_on_records_fetched_greater_than = 1500
-end
